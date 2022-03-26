40af7612565ed024b47625bedc70f3765f3d432c
goosemane <noodlecloudcode@gmail.com>
2022-03-26 09:19:15 -0500

    fixed

--- config.fish
+++ config.fish
@@ -2,8 +2,8 @@ alias gitc="git commit -m "fixed""
 alias c="g++ -o out.exe"
 alias run="~./run.exe"
 alias uu="ls -a"
+alias home="cd ~"
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -g -x fish_greeting ''
-    starship init fish | source
 end
