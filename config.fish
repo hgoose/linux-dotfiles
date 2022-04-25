a65df8cd3b351799225029ee2cd9a5eb58767677
goosemane <noodlecloudcode@gmail.com>
2022-04-25 12:36:19 -0500

    fixed

--- config.fish
+++ config.fish
@@ -1,8 +1,12 @@
 alias gitc="git commit -m "fixed""
 alias c="g++ -o out.exe"
-alias run="~./run.exe"
-alias uu="ls -a"
+alias run="./out.exe"
+alias uuu="ls -a"
+alias uu="ls -l"
 alias home="cd ~"
+alias chrome="/usr/bin/google-chrome-stable"
+alias nochrome="nohup /usr/bin/google-chrome-stable &"
+alias reman="sudo reboot now"
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -g -x fish_greeting ''
