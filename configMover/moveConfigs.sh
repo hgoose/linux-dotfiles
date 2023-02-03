66da90bfe082f8d454b0dd4a90194dd3ba78869e
goosemane <noodlecloudcode@gmail.com>
2023-02-03 13:11:41 -0600

    commit

--- configMover/moveConfigs.sh
+++ configMover/moveConfigs.sh
@@ -13,7 +13,9 @@ echo "Select Option:
 7: zathura
 8: picom
 9: latex
-10: Only push dotfiles
+10: alacritty
+11: Only push dotfiles
+12: Copy all configs
 
 "
 read OPTION
@@ -61,7 +63,28 @@ then
 	
 elif [ "$OPTION" = "10" ];
 then
+	sudo cp ~/.config/alacritty/alacritty.yml ~/linux-dotfiles/alacritty && echo "Copied alacritty config to ~/linux-dotfiles/alacritty" || echo "failed to move config to linux-dotfiles"
+	
+elif [ "$OPTION" = "11" ];
+then
 	sudo git -C ~/linux-dotfiles add . && sudo git -C ~/linux-dotfiles commit -m "changes" && git -C ~/linux-dotfiles push && echo "Pushed changes to github repository Successfully" || echo "Failed to Push changes to github repository"
+
+elif [ "$OPTION" = "12" ];
+then
+	sudo cp ~/.config/fish/config.fish ~/linux-dotfiles/fish && echo "Copied Fish Config to ~/linux-dotfiles/fish" || echo "failed to move config to linux-dotfiles";
+	sudo cp ~/.config/kitty/kitty.conf ~/linux-dotfiles/kitty && echo "Copied Kitty Config to ~/linux-dotfiles/kitty" || echo "failed to move config to linux-dotfiles";
+	sudo cp ~/.config/termite/config ~/linux-dotfiles/termite\ configs && echo "copied termite config to ~/linux-dotfiles/termite" || echo "failed to move config to linux-dotfiles";
+	sudo cp ~/.config/nvim/init.lua ~/linux-dotfiles/astrovim/ && echo "Copied neovim config (init.lua) to ~/linux-dotfiles/astrovim " || echo "falied to move config (init.lua) to linux-dotfiles";
+	sudo cp ~/.config/nvim/lua/core/mappings.lua ~/linux-dotfiles/astrovim/ && echo "Copied neovim config (mappings.lua) to ~/linux-dotfiles/astrovim " || echo "falied to move config (mappings.lua) to linux-dotfiles";
+	sudo cp ~/.config/nvim/lua/core/plugins.lua ~/linux-dotfiles/astrovim/ && echo "Copied neovim config (plugins.lua) to ~/linux-dotfiles/astrovim " || echo "falied to move config (plugins.lua) to linux-dotfiles";
+	sudo cp ~/.config/nvim/lua/core/options.lua ~/linux-dotfiles/astrovim/ && echo "Copied neovim config (options.lua) to ~/linux-dotfiles/astrovim " || echo "falied to move config (options.lua) to linux-dotfiles";
+	sudo cp ~/.config/i3/config ~/linux-dotfiles/i3 && echo "Copied i3 config to ~/linux-dotfiles" || echo "failed to move config to linux-dotfiles";
+	sudo cp ~/.config/i3/i3blocks.conf ~/linux-dotfiles/i3 && echo "Copied i3blocks.conf to ~/linux-dotfiles" || echo "failed to move i3blocks.conf to linux-dotfiles";
+	sudo cp ~/.config/neofetch/config.conf ~/linux-dotfiles/neofetch && echo "Copied neofetch config to ~/linux-dotfiles" || echo "failed to move config to linux-dotfiles";
+	sudo cp ~/.config/zathura/zathurarc ~/linux-dotfiles/zathura && echo "Copied zathura config to ~/linux-dotfiles" || echo "failed to move config to linux-dotfiles";
+	sudo cp ~/.config/picom/picom.conf ~/linux-dotfiles/picom && echo "Copied picom config to ~/linux-dotfiles" || echo "failed to move config to linux-dotfiles";
+	sudo cp ~/.config/nvim/UltiSnips/tex.snippets ~/linux-dotfiles/latex && echo "Copied latex config to ~/linux-dotfiles" || echo "failed to move config to linux-dotfiles";
+	sudo cp ~/.config/alacritty/alacritty.yml ~/linux-dotfiles/alacritty && echo "Copied alacritty config to ~/linux-dotfiles/alacritty" || echo "failed to move config to linux-dotfiles";
 
 else
 	echo "Invalid Option"
