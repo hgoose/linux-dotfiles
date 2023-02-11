c7a5147926325a06c2bc159330f8189f38c8ac41
goosemane <noodlecloudcode@gmail.com>
2023-02-11 09:15:29 -0600

    changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,23 @@
+xset r rate 300 50
+function logo
+    echo '                 '(set_color F00)'___
+  ___======____='(set_color FF7F00)'-'(set_color FF0)'-'(set_color FF7F00)'-='(set_color F00)')
+/T            \_'(set_color FF0)'--='(set_color FF7F00)'=='(set_color F00)')
+[ \ '(set_color FF7F00)'('(set_color FF0)'0'(set_color FF7F00)')   '(set_color F00)'\~    \_'(set_color FF0)'-='(set_color FF7F00)'='(set_color F00)')
+ \      / )J'(set_color FF7F00)'~~    \\'(set_color FF0)'-='(set_color F00)')
+  \\\\___/  )JJ'(set_color FF7F00)'~'(set_color FF0)'~~   '(set_color F00)'\)
+   \_____/JJJ'(set_color FF7F00)'~~'(set_color FF0)'~~    '(set_color F00)'\\
+   '(set_color FF7F00)'/ '(set_color FF0)'\  '(set_color FF0)', \\'(set_color F00)'J'(set_color FF7F00)'~~~'(set_color FF0)'~~     '(set_color FF7F00)'\\
+  (-'(set_color FF0)'\)'(set_color F00)'\='(set_color FF7F00)'|'(set_color FF0)'\\\\\\'(set_color FF7F00)'~~'(set_color FF0)'~~       '(set_color FF7F00)'L_'(set_color FF0)'_
+  '(set_color FF7F00)'('(set_color F00)'\\'(set_color FF7F00)'\\)  ('(set_color FF0)'\\'(set_color FF7F00)'\\\)'(set_color F00)'_           '(set_color FF0)'\=='(set_color FF7F00)'__
+   '(set_color F00)'\V    '(set_color FF7F00)'\\\\'(set_color F00)'\) =='(set_color FF7F00)'=_____   '(set_color FF0)'\\\\\\\\'(set_color FF7F00)'\\\\
+          '(set_color F00)'\V)     \_) '(set_color FF7F00)'\\\\'(set_color FF0)'\\\\JJ\\'(set_color FF7F00)'J\)
+                      '(set_color F00)'/'(set_color FF7F00)'J'(set_color FF0)'\\'(set_color FF7F00)'J'(set_color F00)'T\\'(set_color FF7F00)'JJJ'(set_color F00)'J)
+                      (J'(set_color FF7F00)'JJ'(set_color F00)'| \UUU)
+                       (UU)'(set_color normal)
+end
+logo
+
 # USER DEFINED ALIASES
 alias mch2="cd ~/springsemester/calc/notes/lectures/chapter2"
 alias dnotes="cd ~/springsemester/dmed/notes"
@@ -33,12 +53,18 @@ alias fishc="n ~/.config/fish/config.fish"
 alias kittyc="n ~/.config/kitty/kitty.conf"
 alias pn="cd ~/dev/latex/notes/python/"
 alias picom="nohup picom & && pidof picom | disown"
-alias c="clear && ~/shell-color-scripts/colorscripts/pacman"
-alias C="clear && ~/shell-color-scripts/colorscripts/pacman"
-alias cc="clear && neofetch --source ~/.config/neofetch/logo"
-alias CC="clear && neofetch --source ~/.config/neofetch/logo"
-alias ccc="clear && ~/shell-color-scripts/colorscripts/pacman && neofetch --source ~/.config/neofetch/logo"
-alias CCC="clear && ~/shell-color-scripts/colorscripts/pacman && neofetch --source ~/.config/neofetch/logo"
+alias c="clear && logo" 
+alias C="clear && logo"
+alias cc="clear && logo" 
+alias CC="clear && logo" 
+alias ccc="clear && ~/shell-color-scripts/colorscripts/pacman" 
+alias CCC="clear && ~/shell-color-scripts/colorscripts/pacman" 
+# alias c="clear && ~/shell-color-scripts/colorscripts/pacman"
+# alias C="clear && ~/shell-color-scripts/colorscripts/pacman"
+# alias cc="clear && neofetch --source ~/.config/neofetch/logo"
+# alias CC="clear && neofetch --source ~/.config/neofetch/logo"
+# alias ccc="clear && ~/shell-color-scripts/colorscripts/pacman && neofetch --source ~/.config/neofetch/logo"
+# alias CCC="clear && ~/shell-color-scripts/colorscripts/pacman && neofetch --source ~/.config/neofetch/logo"
 alias x="sudo chmod +x"
 alias dot="cd ~/.config"
 alias nviminit="nvim ~/.config/nvim/init.lua"
@@ -192,10 +218,10 @@ end
 
 
 ### FUNCTIONS ###
-# Fish command history
-function history
-    builtin history --show-time='%F %T ' | sort
-end
+# # Fish command history
+# function history
+#     builtin history --show-time='%F %T ' | sort
+# end
 
 # Make a backup file
 function backup --argument filename
