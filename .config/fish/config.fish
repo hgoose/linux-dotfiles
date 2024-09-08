4f9efd1c2a955d856e522e8b42dfff8951240faa
hgoose <noodlecloudcode@gmail.com>
2024-09-08 15:32:06 -0500

    changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,8 +7,6 @@ set -x PATH $HOME/.cargo/bin $PATH
 set -U fish_greeting
 set fish_vi_key_bindings true
 
-redshift -O 5500
-
 # xrandr --newmode "1440x1080_100.00"  146.25  1680 1784 1960 2240  1050 1053 1059 1089 -hsync +vsync
 # xrandr --addmode HDMI-1-0 1440x1080_100.00
 # xrandr --addmode eDP-1 1440x1080_100.00
