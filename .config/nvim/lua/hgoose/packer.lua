66807669802def67935a274eef5004f9fa5d2291
hgoose <noodlecloudcode@gmail.com>
2025-03-06 12:21:26 -0600

    changes

--- .config/nvim/lua/hgoose/packer.lua
+++ .config/nvim/lua/hgoose/packer.lua
@@ -7,9 +7,9 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' }},
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.6',
+        requires = { {'nvim-lua/plenary.nvim'},
         require("telescope").setup{
             pickers = {
                 colorscheme = {
@@ -17,7 +17,19 @@ return require('packer').startup(function(use)
                 }
             }
         }
-    }
+    }}
+    -- use {
+    --     'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    --     -- or                            , branch = '0.1.x',
+    --     requires = { { 'nvim-lua/plenary.nvim' }},
+    --     require("telescope").setup{
+    --         pickers = {
+    --             colorscheme = {
+    --                 enable_preview = true
+    --             }
+    --         }
+    --     }
+    -- }
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     -- use('nvim-treesitter/playground')
     use('ThePrimeagen/harpoon')
